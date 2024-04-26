3d0691e0512dae978107ac13cdd55613a5cbfb71
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-04-27 01:00:07 +0500

    Добавьте файлы проекта.

---
+++ ДЗ. Магазин кристалов/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace ДЗ.Магазин_кристалов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals;
+            int oneCrystalPrice = 15;
+
+            Console.WriteLine("Вас приветствует магазин кристалов! Сегодня у нас действует акция! Один кристал за " + oneCrystalPrice + " золота.");
+            Console.Write("Сколько у вас золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Сколько кристалов вам необходимо? ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            gold-= crystals*oneCrystalPrice;
+            Console.WriteLine($"У вас в сумке {crystals} кристалов, и {gold} едениц золота");
+        }
+    }
+}
